b5631448b5a8d1536d3baa6d9531b107c63f1013
webwill <williamjohnwall@gmail.com>
2021-09-18 01:50:52 +0100

    Setting up the app

---
+++ app/src/main/java/com/webwill/mathsgame/MainActivity.java
@@ -0,0 +1,14 @@
+package com.webwill.mathsgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
